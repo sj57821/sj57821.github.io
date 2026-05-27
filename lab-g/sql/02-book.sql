f6c5e01a478e664de425d76e6d82caf7174e9da4
sj57821 <sj57821@zut.edu.pl>
2026-05-27 19:09:36 +0200

    Delete lab-g directory

--- lab-g/sql/02-book.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE books (
-                       id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       title TEXT NOT NULL,
-                       author TEXT NOT NULL,
-                       year INTEGER NOT NULL,
-                       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
